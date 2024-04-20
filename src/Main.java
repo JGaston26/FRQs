4d9fb9884840d1043ec5cfe9b2c0556548b4dc6a
shifat <Shifatulg@gmail.com>
2024-04-20 19:58:08 -0400

    PUZZLE 8 WIP NOT COMPLETE

--- src/Main.java
+++ src/Main.java
@@ -1,29 +1,40 @@
 public class Main {
     public static void main(String[] args) {
-        String str1 = "      "; // 6 F
-        String str2 = " "; // 1 A
-        String str3 = "         "; // 9 I
-        String str4 = "            "; // 12 L
-        String str5 = "                     "; // 21 U
-        String str6 = "                  "; // 18 R
-        String str7 = "     "; // 5 E
-        String str8 = "         "; // 9 I
-        String str9 = "                   "; // 19 S
-        String str10 = "                    "; // 20 T
-        String str11 = "        "; // 8 H
-        String str12 = "     "; // 5 E
-        System.out.println(str1.length());
-        System.out.println(str2.length());
-        System.out.println(str3.length());
-        System.out.println(str4.length());
-        System.out.println(str5.length());
-        System.out.println(str6.length());
-        System.out.println(str7.length());
-        System.out.println(str8.length());
-        System.out.println(str9.length());
-        System.out.println(str10.length());
-        System.out.println(str11.length());
-        System.out.println(str12.length());
+        System.out.println("\nNode 1\n_______________");
+        System.out.println(nodeInfo(node1));
+        System.out.println("\nNode 2\n_______________");
+        System.out.println(nodeInfo(node2));
+        System.out.println("\nNode 3\n_______________");
+        System.out.println(nodeInfo(node3));
+        System.out.println("\nNode 4\n_______________");
+        System.out.println(nodeInfo(node4));
+        System.out.println("\nNode 5\n_______________");
+        System.out.println(nodeInfo(node5));
+        System.out.println("\nNode 6\n_______________");
+        System.out.println(nodeInfo(node6));
+        System.out.println("\nNode 7\n_______________");
+        System.out.println(nodeInfo(node7));
+    }
+    static Nodes node1 = new Nodes(3486,3513,-154);
+    static Nodes node2 = new Nodes(2895,-2142,-443);
+    static Nodes node3 = new Nodes(-3461,126,-600);
+    static Nodes node4 = new Nodes(3341,345,0);
+    static Nodes node5 = new Nodes(3236,3701,-1992);
+    static Nodes node6 = new Nodes(4574,-2881,4970);
+    static Nodes node7 = new Nodes(-1170,1606,-4100);
+    public static String nodeInfo(Nodes node) {
+        String str = "";
+        str += "\nNode1: " + nodeDistance(node, node1);
+        str += "\nNode2: " + nodeDistance(node, node2);
+        str += "\nNode3: " + nodeDistance(node, node3);
+        str += "\nNode4: " + nodeDistance(node, node4);
+        str += "\nNode5: " + nodeDistance(node, node5);
+        str += "\nNode6: " + nodeDistance(node, node6);
+        str += "\nNode7: " + nodeDistance(node, node7);
+        return str;
+    }
 
+    public static double nodeDistance(Nodes nod1, Nodes nod2) {
+        return (Math.sqrt(Math.pow(nod1.getX() - nod2.getX(),2) + Math.pow(nod1.getY() - nod2.getY(),2) + Math.pow(nod1.getZ() - nod2.getZ(),2)));
     }
 }
---
+++ src/Nodes.java
@@ -0,0 +1,20 @@
+public class Nodes {
+    private int x;
+    private int y;
+    private int z;
+    public Nodes(int x, int y, int z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    public int getX() {
+        return x;
+    }
+    public int getY() {
+        return y;
+    }
+    public int getZ() {
+        return z;
+    }
+}
