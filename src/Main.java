9e86dba35684da50b393c44928bb3451b5a96857
shifat <Shifatulg@gmail.com>
2024-04-20 11:00:59 -0400

    PUZZLE 1 COMPLETE

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,29 @@
 public class Main {
     public static void main(String[] args) {
-        for (int skip = 1; skip < 82; skip++) {
-            String temp = "";
-            String a = "ewibbzzqfxtmjfbosptuwihdiqufxeakqglzvjuicumkmdfzkwybeounpjkffdclumvhbrrvkywtsnikawnqinzvzgpzdukoyzctqemklmgrzapxazjygqzhtcjyvmrjqabtevuwfelpggvwmsoivjurwghoeaaqdl";
-            for (int i = 0; i < a.length(); i += skip) {
-                temp += a.substring(i, i + 1);
-            }
-            System.out.println(temp);
-        }
+        String str1 = "      "; // 6 F
+        String str2 = " "; // 1 A
+        String str3 = "         "; // 9 I
+        String str4 = "            "; // 12 L
+        String str5 = "                     "; // 21 U
+        String str6 = "                  "; // 18 R
+        String str7 = "     "; // 5 E
+        String str8 = "         "; // 9 I
+        String str9 = "                   "; // 19 S
+        String str10 = "                    "; // 20 T
+        String str11 = "        "; // 8 H
+        String str12 = "     "; // 5 E
+        System.out.println(str1.length());
+        System.out.println(str2.length());
+        System.out.println(str3.length());
+        System.out.println(str4.length());
+        System.out.println(str5.length());
+        System.out.println(str6.length());
+        System.out.println(str7.length());
+        System.out.println(str8.length());
+        System.out.println(str9.length());
+        System.out.println(str10.length());
+        System.out.println(str11.length());
+        System.out.println(str12.length());
+
     }
 }
